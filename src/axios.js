263e8dba466aab4f19eaea5755934e068d2f981f
CORP\buyantogtokh.alt <togtokh.dev@gmail.com>
2022-09-16 06:33:38 +0800

    main

--- src/axios.js
+++ src/axios.js
@@ -2,3 +2,4 @@ import axios from "axios";
 import { getCookie } from "./functions/cookie";
 axios.defaults.baseURL = import.meta.env.VITE_APP_BACK_END_URL;
 axios.defaults.headers.common["Authorization"] = "Bearer " + getCookie("token");
+axios.defaults.headers.common["Accept-Language"] = "admin";
