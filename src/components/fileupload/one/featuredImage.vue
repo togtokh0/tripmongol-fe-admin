99d7682cd91a20e5d26e0f4d75e2b65d0d883baf
CORP\buyantogtokh.alt <togtokh.dev@gmail.com>
2022-09-19 10:47:07 +0800

    main

--- src/components/fileupload/one/featuredImage.vue
+++ src/components/fileupload/one/featuredImage.vue
@@ -189,7 +189,7 @@ export default {
         method: "post",
         url: import.meta.env.VITE_APP_BACK_END_FILE_URL + "/api/v1/upload/file",
         headers: {
-          Authorization: "Basic aG9saWRheXM6cGFzc3dvcmQ=",
+          Authorization: "Basic dHJpcG1vbmdvbDpwYXNzd29yZA==",
         },
         data: formData,
       };
